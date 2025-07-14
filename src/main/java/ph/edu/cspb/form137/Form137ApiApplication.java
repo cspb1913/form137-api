ccd7f5a09176268699f8adb5d85db8495cf9f46d
Jason Calalang <jcalalang@ph.ibm.com>
2025-07-14 14:41:58 +0800

    Initial commit

---
+++ src/main/java/ph/edu/cspb/form137/Form137ApiApplication.java
@@ -0,0 +1,13 @@
+package ph.edu.cspb.form137;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Form137ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Form137ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ph/edu/cspb/form137/Form137ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.edu.cspb.form137;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Form137ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
