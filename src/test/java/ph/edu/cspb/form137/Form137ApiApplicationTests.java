628dc07b2c0ba5c98e37ed690c6e47f78b929611
Jason Calalang <jcalalang@ph.ibm.com>
2025-07-14 14:53:54 +0800

    fix build and tests

--- src/test/java/ph/edu/cspb/form137/Form137ApiApplicationTests.java
+++ src/test/java/ph/edu/cspb/form137/Form137ApiApplicationTests.java
@@ -3,11 +3,18 @@ package ph.edu.cspb.form137;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+/**
+ * Basic context load test.
+ * <p>Embedded MongoDB auto-configuration is disabled to avoid
+ * downloading the MongoDB binary during tests, which can fail in
+ * restricted environments.</p>
+ */
+@SpringBootTest(properties =
+        "spring.autoconfigure.exclude=" +
+        "de.flapdoodle.embed.mongo.spring.autoconfigure.EmbeddedMongoAutoConfiguration")
 class Form137ApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
